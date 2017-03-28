241e40913e25a579dd45c731164aa7787c8f83e7
Gene Higgins <ehigginsiii@cnm.edu>
2017-03-28 17:01:26 -0600

    styling navbar

--- src/plonetheme/tango/theme/js/scripts.js
+++ src/plonetheme/tango/theme/js/scripts.js
@@ -1,6 +1,26 @@
     $(document).ready(function($) {
     	console.clear();
         $(window).scroll(function(event) {
-            console.log($(window).scrollTop());
+            let pxToTop = $(window).scrollTop();
+            
+            // if (pxToTop > 120) {
+            //     console.log('greater')
+            //     $('.navbar-nav li').animate({'padding-top': '2em'}, "slow")
+            // } else if (pxToTop <= 120) {
+            //     console.log('lesser')
+            //     $('.navbar-nav li').animate({'padding-top': '5.5em'}, "slow")
+            // }
+            if (pxToTop > 120) {
+                $('.navbar-nav li').css('padding-top', '2em')
+            }
+            if (pxToTop < 120) {
+                console.log(pxToTop);
+                $('.navbar-nav li').css('padding-top', '5.5em')
+            }
         });
+        $("#carousel-example-generic").carousel();
+
+        $( ".right.carousel-control" ).click(function() {
+        	//console.log('clicked');
+        })
     });
