3f0de0d9f910de0917afab14d9678d3f7169c5f1
Gene Higgins <ehigginsiii@cnm.edu>
2017-04-06 11:27:51 -0600

    theme won't load :-

--- src/plonetheme/tango/theme/js/scripts.js
+++ src/plonetheme/tango/theme/js/scripts.js
@@ -1,7 +1,7 @@
     $(document).ready(function($) {
     	console.clear();
         $(window).scroll(function(event) {
-            let pxToTop = $(window).scrollTop();
+            var pxToTop = $(window).scrollTop();
             
             // if (pxToTop > 120) {
             //     console.log('greater')
@@ -14,7 +14,6 @@
                 $('.navbar-nav li').css('padding-top', '2em')
             }
             if (pxToTop < 120) {
-                console.log(pxToTop);
                 $('.navbar-nav li').css('padding-top', '5.5em')
             }
         });
@@ -26,4 +25,37 @@
 
         // don't display text in icon span tags
         $('.counter-type-1 .icon').empty();
+
+
+
+        // generic counter
+        ( function counters() {
+
+            var sayHello = function(x){
+                console.log(x);
+            }
+
+            var array = [];
+
+            array.forEach.call(document.getElementsByClassName("counter"), function(el){
+
+                    var _from = parseInt(el.dataset.from);
+                    var _to = parseInt(el.dataset.to);
+                    if (el.dataset.speed) var _speed = parseInt(el.dataset.speed);
+                    var currentVal = el.textContent = _from;
+
+                    var loop = setInterval(function(){
+                        console.log(currentVal);
+                        el.innerHTML = currentVal++;
+                        if (currentVal >= _to) {
+                            clearInterval(loop);
+                            el.innerHTML = _to;
+                        }
+                    }, _speed / 1000);
+
+
+
+
+             })
+         })();
     });
